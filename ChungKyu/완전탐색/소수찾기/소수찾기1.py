f46dc032a941f0fe19591c1c19897aa71a8191e8
ck <dbcjdrb@naver.com>
2022-02-28 08:56:01 +0900

    <2월 28일 > 모의고사 소수찾기 카펫

---
+++ ChungKyu/완전탐색/소수찾기/소수찾기1.py
@@ -0,0 +1,19 @@
+import math
+
+# -*- coding: utf-8 -*-
+
+def primenumber1(x):
+    for i in range (2, int(math.sqrt(x)+1)):
+        if x % i == 0:
+            return False
+        return True
+
+print(primenumber1(10))
+
+
+
+# def primenumber(x):
+#     for i in range(2,x):
+#         if x % i == 0:
+#             return False
+#         return  True
