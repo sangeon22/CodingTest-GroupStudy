172f8baf738ebae155e0b54e68526816d1f21902
sangeon22 <tkddjsdl33@hanmail.net>
2022-02-27 23:28:13 +0900

    2022-02-27 프로그래머스 > 완전 탐색 > 카펫, 모의고사, 소수찾기

---
+++ SangEon/소수찾기/ProgrammersPrimeNumber.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+def solution(numbers):
+    answer = []
+    num1 = [n for n in numbers]
+    per = []
+
+    for i in range(1, len(numbers)+1):
+        per += list(permutations(num1,i))
+    num2 = [int(("").join(j)) for j in per]
+
+    for n in num2:
+        if n<2:
+            continue
+        check = True
+        for i in range(2,int(n**0.5)+1):
+            if n % i == 0:
+                check = False
+                break
+        if check:
+            answer.append(n)
+
+    return len(set(answer))
