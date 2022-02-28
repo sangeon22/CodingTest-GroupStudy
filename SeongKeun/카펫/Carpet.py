4c74eeac0f71a73f071fe297341392160a902351
tjdrms <84035809+tjdrms@users.noreply.github.com>
2022-02-28 11:00:41 +0900

    카펫 문제풀이
    카펫 문제풀이입니다

---
+++ SeongKeun/카펫/Carpet.py
@@ -0,0 +1,11 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow                  
+    for b in range(1,total+1):
+        if (total / b) % 1 == 0:    
+            a = total / b
+            if a >= b:                      
+                if 2*a + 2*b == brown + 4:   
+                    return [a,b]
+            
+    return answer
